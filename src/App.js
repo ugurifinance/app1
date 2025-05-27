65d87db51a53db1855402081a4c824e9f5fa781b
Ugur <ukurbanov@ifinancecanada.com>
2025-05-27 12:41:06 -0400

    init commit for components

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn REACT 4
+          Learn React
         </a>
       </header>
     </div>
